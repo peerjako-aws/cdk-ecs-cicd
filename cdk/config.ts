167836162b0cfdc314b87c7aa6737227fe3e6d9c
Jakobsen <peerjako@38f9d34e66c7.ant.amazon.com>
2019-05-25 23:46:12 +0200

    Use dev branch for dev pipeline

--- cdk/config.ts
+++ cdk/config.ts
@@ -1,6 +1,6 @@
 export const githubOwner = 'peerjako-aws';
 export const repoName = 'cdk-ecs-cicd';
-export const gitDevBranch = 'master'
+export const gitDevBranch = 'dev'
 export const gitProdBranch = 'master'
 export const awsSecretsGitHubTokenName = 'github-personal-access-token';
 export const ssmImageTagParamName = 'penguinapp-imagetag';
